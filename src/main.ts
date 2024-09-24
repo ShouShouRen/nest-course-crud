111b52a6d7d6cc6499b29fd4ce5d5637323c891a
Peter Chen <peterchen2017125@gmail.com>
2024-09-24 22:55:50 +0800

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseIntercept } from './common/responseIntercept';
 import { AbnormalFilter } from './common/abnormalFilter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseIntercept());
 
   app.useGlobalFilters(new AbnormalFilter());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
