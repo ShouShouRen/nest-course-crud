796f377d727fe80cd22a5a5fb790eaf5a50b6d4e
Peter Chen <peterchen2017125@gmail.com>
2024-09-24 22:17:40 +0800

    refactor: remove unused app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config';
 import { CourseModule } from './course/course.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, CourseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, CourseModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
